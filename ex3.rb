c997cc864e5ebc6c7cb9461f7ddc3529f06ebd3f
Cena Mayo <cenazoic@gmail.com>
2014-10-21 09:13:35 -0500

    Exercise 3 with study drill additions"

---
+++ ex3.rb
@@ -0,0 +1,33 @@
+#print the following text
+puts "I will now count my chickens: "
+
+#print the number of hens and roosters
+puts "Hens #{25.0 + 30.0 / 6.0}"
+puts "Roosters #{100.0-25.0*3.0%4.0}"
+
+#print the following
+puts "Now I will count the eggs:"
+
+#calculate number of eggs
+puts 3.0+2.0+1.0-5.0+4.0%2.0-1.0/4.0+6.0
+
+#print the following
+puts "Is it true that 3+2 < 5-7?"
+
+#calculate 
+puts 3.0+2.0<5.0-7.0
+
+#calculate
+puts "What is 3 + 2? #{3.0+2.0}"
+puts "What is 5 -7? #{5.0-7.0}"
+
+#print
+puts "Oh, that's why it's false."
+
+#print
+puts "How about some more."
+
+#print/calculate booleans
+puts "Is it greater? #{5.0> -2.0}"
+puts "Is it greater or equal? #{5.0 >= -2.0}"
+puts "Is it less or equal? #{5.0 <= -2.0}"
