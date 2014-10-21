bbef7ccdd961a1078a8947d28d4e39c11641e5f9
Cena Mayo <cenazoic@gmail.com>
2014-10-21 09:55:39 -0500

    ZedRuby ex. 5

---
+++ ex5.rb
@@ -0,0 +1,16 @@
+my_name = 'Cena Mayo'
+my_age = 1 # the first of many lies
+my_height = 85 # in inches
+my_weight = 100 # in lbs
+my_eyes = 'Brown'
+my_teeth = 'White'
+my_hair = 'Auburn'
+
+puts "Let's talk about #{my_name}."
+puts "She is #{my_height} inches tall. Seems unlikely."
+puts "She is #{my_weight} pounds heavy."
+puts "Actually that seems even more unlikely."
+puts "She's got #{my_eyes} and #{my_hair} hair."
+puts "Her teech are usually #{my_teeth} depending on the coffee."
+
+puts "If I add #{my_age}, #{my_height}, and #{my_weight} I get #{my_age + my_height + my_weight}."
